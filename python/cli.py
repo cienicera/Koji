c5c88f1629b122375cc6861a3cf3303c3ec2dcbd
Aryan Godara <aryangodara03@gmail.com>
2024-03-27 17:27:28 +0530

    minor fixes related to set_tempo

--- python/cli.py
+++ python/cli.py
@@ -10,15 +10,13 @@ def main():
     args = parser.parse_args()
 
     if args.format == 'cairo':
-        # Assuming the input is always a MIDI file when converting to cairo or json
         midi_to_cairo_struct(args.input_file, args.output_file)
         print(f"Converted {args.input_file} to Cairo format in {args.output_file} ✅")
     elif args.format == 'json':
         midi_to_json(args.input_file, args.output_file)
         print(f"Converted {args.input_file} to JSON format in {args.output_file} ✅")
     elif args.format == 'midi':
-        # Assuming the input for midi format is a structured format or json that needs to be converted back to MIDI
-        cairo_struct_to_midi(args.input_file, args.output_file)  # Implement this functionality based on your data structure
+        cairo_struct_to_midi(args.input_file, args.output_file)
         print(f"Converted {args.input_file} from Cairo/JSON format back to MIDI in {args.output_file} ✅")
 
 if __name__ == '__main__':
